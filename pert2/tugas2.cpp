e2707ad2e696f0108ee2c49a78a1d8da3b379016
root <root@DESKTOP-PEBD877.localdomain>
2021-10-05 20:09:26 +0700

    tugas

---
+++ pert2/tugas2.cpp
@@ -0,0 +1,16 @@
+#include <iostream> ;
+using namespace std ;
+
+int main () {
+	float tahun, umur ;
+	
+	cout<<"menghitung umur"<<endl ;
+	cout << "---------------------"<<endl ;
+	cout <<"masukan tahun kelahiran anda\t\t :" ;
+	cin>>tahun ;
+	umur= 2022-tahun ;
+	cout<< "umur saya sekarang"<<umur<< "tahun \n" ;
+	
+	return 0;
+	
+}
